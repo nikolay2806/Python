94769330e3e180e4bdbca3c4bd1cd78363679694
nikolay2806 <kolya2806@gmail.com>
2023-08-14 11:41:06 +0300

    1

---
+++ 1_variables.py
@@ -0,0 +1,22 @@
+#Объявление переменных
+name = 'Boeing'
+model = '737 MAX 8'
+year = 2017
+klass = True
+distance = 6510
+price = 52.85
+
+#Объявление списка
+list_airplane = [name, model ,year ,klass ,distance ,price]
+
+#Описание переменой
+print('Название самолета:',name, '\nМодель:',model, '\nГод:',year , '\nKласс пасажирский:', klass, '\nДальность полета:',distance ,'\nСтоимость:', 52.85, 'миллиона\n')
+
+#Вывод типа перменных
+print(list_airplane)
+print(type(name))
+print(type(model))
+print(type(year))
+print(type(klass))
+print(type(distance))
+print(type(price))
